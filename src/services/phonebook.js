dee1a5c084040492bb4a4e71840d430db2c189c9
coscoaj1 <coscoanthony1@gmail.com>
2021-12-12 09:47:49 -0500

    prod build

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/people";
+const baseUrl = "/api/people";
 
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
