a08c16af1a7972f78d6a260a875b7eb08221fdca
coscoaj1 <coscoanthony1@gmail.com>
2021-12-14 07:19:22 -0500

    testing

--- src/components/Person.test.js
+++ src/components/Person.test.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
-import { prettyDOM } from '@testing-library/dom';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import { prettyDOM } from "@testing-library/dom";
 
-import Person from './Person';
+import Person from "./Person";
 
-test('renders content', () => {
-	const person = {
-		name: 'Component testing is done with react-testing-library',
-		phoneNumber: '828-555-1212',
-	};
+test("renders content", () => {
+  const person = {
+    name: "Component testing is done with react-testing-library",
+    phoneNumber: "828-555-1212",
+  };
 
-	const view = render(<Person person={person} />);
+  const component = render(<Person person={person} />);
 
-	expect(view.container).toHaveTextContent(
-		'Component testing is done with react-testing-library'
-	);
-	const button = view.container.querySelector('button');
+  expect(component.container).toHaveTextContent(
+    "Component testing is done with react-testing-library"
+  );
+  const button = component.container.querySelector("button");
 
-	console.log(prettyDOM(button));
+  console.log(prettyDOM(button));
 });
 
-test('clicking the button calls the event handler once', () => {
-	const person = {
-		name: 'Component testing is done with react-testing library',
-		phoneNumber: '828-555-1212',
-	};
+test("clicking the button calls the event handler once", () => {
+  const person = {
+    name: "Component testing is done with react-testing library",
+    phoneNumber: "828-555-1212",
+  };
 
-	const mockHandler = jest.fn();
+  const mockHandler = jest.fn();
 
-	const view = render(<Person person={person} handleDelete={mockHandler} />);
+  const component = render(
+    <Person person={person} handleDelete={mockHandler} />
+  );
 
-	const button = view.getByText('Delete');
-	fireEvent.click(button);
+  const button = component.getByText("Delete");
+  fireEvent.click(button);
 
-	expect(mockHandler.mock.calls).toHaveLength(1);
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
